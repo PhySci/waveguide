ca4f8a26ebe4fdcf979d696d3572f9496fdbccea
Fedor Mushenok <mushenokf@gmail.com>
2016-01-24 23:35:37 +0000

    First commit

---
+++ test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 16 18:28:18 2016
+
+@author: SuperMan
+"""
+
+from waveguide import waveguideClass
+
+Wg = waveguideClass()
+
+print(Wg.Kq(1))
---
+++ waveguide.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Class descibes uniform anisotropic magnetic waveguide
+
+Methods are intended for calculation of eigen spectra of spin waves
+
+@author: SuperMan
+"""
+
+import math
+
+class waveguideClass:
+
+    M0 = 1  # magnetization (units?)    
+    w = 1   # width (units?)
+    L = 1   # thickness (units?)
+    H = 1000 # external magnetic field (units)
+       
+    
+    
+    # constructor
+    def __init__(self):
+        pass
+
+    #TODO: create lists of available wave vecors Kq and Kn, calculate then in _init_ method
+    
+    # available wave vectors
+    def Kq(self,q):
+        return  4*math.pi/(q*self.L)
+
+    # available wave vectorss
+    def Kn(self,n):
+        return  4*math.pi/(n*self.w)
+        
+    #TODO: create lists of values Dq and Dn
+    def Dq(self,q):
+        return 1
+        
+    def Dn(self,n):
+        return 1 
+    
+    def C1(self,k,q):
+        kq = self.Kq(self,q)
+        return 2/(k^2+kq^2)*( k*math.cos(0.5*k*self.w)*math.sin(0.5*kq*self.w)-kq*math.sin(0.5*k*self.w)*math.cos(0.5*kq*self.w))
+        
+    
+    def C2(self,k,q):
+        kq = self.Kq(self,q)
+        return 2/(k^2+kq^2)*( k*math.sin(0.5*k*self.w)*math.cos(0.5*kq*self.w)-kq*math.cos(0.5*k*self.w)*math.sin(0.5*kq*self.w))
+        
+    def Fzero(self,q):
+        return 1
+        
+    def I1(self,k,q1,q2):
+        return self.Fzero(q1)*self.Fzero(q2)*self.C2()
+    
+    def I2(self,k,q1,q2):
+        pass
+    
+        
+    
+
+        
+        
+    
+        
+        
